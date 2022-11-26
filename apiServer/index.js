6a28aab3d4f1fcbf2bab3f0f6aeca89a3dce346b
Berk Coşar <44787272+cosarberk@users.noreply.github.com>
2022-11-27 02:04:01 +0300

    psql connection info changed

--- apiServer/index.js
+++ apiServer/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const db = require('./queries')
-const port = 3001
+const port = 3005
 const path = require('path')
 const cors = require('cors')
 
